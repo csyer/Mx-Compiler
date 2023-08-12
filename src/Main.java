c1c6585b868a0f62c1b0d11c195c1878c0c6674e
csyer <2960069894@qq.com>
2023-08-12 16:36:02 +0800

    oj test

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
+// import java.io.FileOutputStream;
+
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
+// import IR.IRProgram;
 import ast.*;
+// import backend.IRBuilder;
 import frontend.*;
 import parser.*;
 import utils.*;
-import utils.Error;
+// import utils.Error;
 import semantic.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        try {
+        // try {
             MxLexer lexer = new MxLexer(CharStreams.fromStream(System.in));
             lexer.removeErrorListeners();
             lexer.addErrorListener(new MxErrorListener());
@@ -21,16 +25,21 @@ public class Main {
             ParseTree tree = parser.program();
             ASTBuilder astBuilder = new ASTBuilder();
             ProgramNode ast = (ProgramNode) astBuilder.visit(tree);
-            // ast.debug();
 
             globalScope globalScope = new globalScope(null);
             new SymbolCollector(globalScope).visit(ast);
             new SemanticChecker(globalScope).visit(ast);
-        } catch ( Error err ) {
-            System.err.println("Fail");
-            System.out.println(err.toString());
-            return ;
-        } 
-        System.err.println("Success");
+
+            // IRProgram irProgram = new IRProgram();
+            // new IRBuilder(irProgram, globalScope).visit(ast);
+            // FileOutputStream irOut = new FileOutputStream("test.ll");
+            // irOut.write(irProgram.toString().getBytes());
+            // irOut.close();
+        // } catch ( Error err ) {
+        //     System.err.println("Fail");
+        //     System.out.println(err.toString());
+        //     return ;
+        // } 
+        // System.err.println("Success");
     }
 }
